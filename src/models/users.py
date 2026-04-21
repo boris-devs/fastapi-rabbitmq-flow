b800c490ea41841f26e417109f39a539d71fc32e
xborismenx <boris.tikhonov.21@gmail.com>
2026-04-21 20:43:09 +0300

    Add `Users` model with `id`, `username`, `email`, and `password_hash` fields

---
+++ src/models/users.py
@@ -0,0 +1,13 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.models import Base
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
