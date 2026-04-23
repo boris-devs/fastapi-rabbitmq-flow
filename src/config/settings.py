d9cf2345fc3ad7a32fd94307192f67c5915c634f
xborismenx <boris.tikhonov.21@gmail.com>
2026-04-24 01:07:53 +0300

    Add JWT config variables to settings and update config module initialization

--- src/config/settings.py
+++ src/config/settings.py
@@ -8,6 +8,12 @@ class Settings(BaseSettings):
     POSTGRES_PORT: int = 5432
     POSTGRES_HOST: str = "localhost"
 
+    ACCESS_SECRET_KEY: str = "access"
+    REFRESH_SECRET_KEY: str = "refresh"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 60 * 24 * 7
+
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
     @property
