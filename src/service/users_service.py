a12e42a4718669c3e32c1ffde5ba519162347216
xborismenx <boris.tikhonov.21@gmail.com>
2026-04-26 00:29:28 +0300

    Add docstrings to user service methods and implement `user_profile` for fetching user details

--- src/service/users_service.py
+++ src/service/users_service.py
@@ -15,6 +15,20 @@ class UserService:
 		self.user_repo = user_repo
 
 	async def register_new_user(self, data: UserCreateRequestSchema):
+		"""
+		Registers a new user in the database. Validates if a user with the given username or
+		email already exists. If a user exists, raises an HTTP exception indicating the conflict.
+		If the registration process encounters database issues or other errors, appropriate
+		exceptions are raised.
+
+		:param data: User creation details containing username, email, and password.
+		:type data: UserCreateRequestSchema
+		:return: The newly created user object if the registration succeeds.
+		:rtype: User
+		:raises HTTPException: If a user with the specified username or email already exists,
+		    if a database integrity error occurs during the operation, or if another issue
+		    is encountered.
+		"""
 		exist_user = await self.user_repo.get_user_by_username_or_email(data.username, data.email)
 		if exist_user:
 			message = (
@@ -39,6 +53,27 @@ class UserService:
 			raise e
 
 	async def login_user(self, data: UserLoginRequestSchema):
+		"""
+		Authenticates the user using the provided email and password and generates
+		access and refresh tokens upon successful login. This method validates
+		the user's credentials, checks if the email exists, and ensures the
+		provided password matches the stored password hash. Upon successful
+		validation, it returns a dictionary containing the access token,
+		refresh token, and token type.
+
+		:param data: An instance of "UserLoginRequestSchema" containing the
+		    user's login credentials, including email and password.
+		:type data: UserLoginRequestSchema
+
+		:return: A dictionary containing the generated access token, refresh
+		    token, and token type.
+		:rtype: dict
+
+		:raises HTTPException: If the user does not exist or the provided email
+		    and password do not match, raises an HTTP 401 error indicating
+		    unauthorized access.
+
+		"""
 		user = await self.user_repo.get_user_by_email(data.email)
 		if not user:
 			raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect email or password")
@@ -52,3 +87,9 @@ class UserService:
 		        "refresh_token": refresh_token,
 		        "token_type": "bearer"
 		        }
+
+	async def user_profile(self, user_id: int):
+		user = await self.user_repo.get_user_by_id(user_id)
+		if not user:
+			raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+		return user
