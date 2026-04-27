2911545f6bf6272bbaa48162dfe14525f328e403
xborismenx <boris.tikhonov.21@gmail.com>
2026-04-27 17:46:36 +0300

    Integrate RabbitMQ connection lifecycle management with FastAPI using lifespan handler

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
-from src.routers import users_router
-app = FastAPI()
+from contextlib import asynccontextmanager
 
+from fastapi import FastAPI
+
+from broker.rabbitmq_manager import RabbitMQManager
+from src.config import settings
+from src.routers import users_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+	pika_connection = RabbitMQManager(settings.RABBITMQ_AMQP_URL)
+	await pika_connection.connect()
+	app.state.rmq = pika_connection
+	yield
+	await pika_connection.close()
+
+
+app = FastAPI(lifespan=lifespan)
 
 prefix = "/api"
 app.include_router(router=users_router, prefix=f"{prefix}/users", tags=["users"])
