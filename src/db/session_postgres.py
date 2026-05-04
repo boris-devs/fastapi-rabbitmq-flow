6165cb9c5d1062ccf6f37308203b1db9314e8e08
xborismenx <boris.tikhonov.21@gmail.com>
2026-05-04 10:37:59 +0300

    refactor: remove unused `sessionmaker` import from PostgreSQL session configuration

--- src/db/session_postgres.py
+++ src/db/session_postgres.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
-from sqlalchemy.orm import sessionmaker
 
 from src.config import settings
 
